2f699e74e4af889475382754ba927d8852386f45
AnjiBarik <cssjavascr@gmail.com>
2024-05-17 15:30:47 +0300

    depl

--- emarket/src/App.js
+++ emarket/src/App.js
@@ -14,28 +14,6 @@ import Search from "./components/book-list/Search";
 import LandingPage from "./components/landingPage/LandingPage";
 
 function App() {
-//   const {setBooks}=useContext(BooksContext)
-//   const { data, loading, error } = useGoogleSheets({
-//     apiKey: 'AIzaSyAtOlmWupFuYG03En08zp5j4KLBo46t7iQ',
-//     sheetId: '1PbTv6NS73mHek4a_SJ5OY8ciHrZMEIZmqE2H5i-jQDY',
-//     sheetsOptions: [{ id: 'Sheet1' }],
-//   });
-  
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error!</div>;
-//   }
-
-
-// const jsonData =  (data[0].data);
- 
-
-
-//  setBooks(jsonData)
-
 
 return (
   <Routes>
