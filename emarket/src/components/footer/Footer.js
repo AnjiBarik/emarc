fc9822dd7e9738a781648fa90851d244baec5a48
AnjiBarik <cssjavascr@gmail.com>
2024-07-16 10:29:54 +0300

    aversion

--- emarket/src/components/footer/Footer.js
+++ emarket/src/components/footer/Footer.js
@@ -60,22 +60,23 @@ export default function Footer() {
         <section className={theme}>
           <section className="foot">
             <section className="footer">
-              <section className="cart">
-                <Link className="cart" to="/">
+              <section className={`cart ${selectedImage === 'home' ? 'sel' : ''}`}>
+                <Link className="cart" to="/">                
                   <img
-                    className={`back-button ${selectedImage === 'home' ? 'sel' : ''}`}
+                    className="back-button"
                     onClick={() => handleImageClick('home')}
                     src={home}
                     alt="home"
                   />
                   {fieldState.home && <span className="button-label">{fieldState.home}</span>}
+               
                 </Link>
               </section>
 
-              <section className="cart">
+              <section className={`cart ${selectedImage === 'category' ? 'sel' : ''}`}>
                 <Link className="cart" to="/BookList">
                   <img
-                    className={`back-button ${selectedImage === 'category' ? 'sel' : ''}`}
+                    className="back-button"
                     onClick={() => handleImageClick('category')}
                     src={category}
                     alt="category"
@@ -84,10 +85,10 @@ export default function Footer() {
                 </Link>
               </section>
 
-              <section className="cart">
+              <section className={`cart ${selectedImage === 'cart' ? 'sel' : ''}`}>
                 <Link className="cart" to="/cart">
                   <img
-                    className={`back-button ${selectedImage === 'cart' ? 'sel' : ''}`}
+                    className="back-button"
                     onClick={() => handleImageClick('cart')}
                     src={carticon}
                     alt="cart"
@@ -99,10 +100,10 @@ export default function Footer() {
                 </Link>
               </section>
 
-              <section className="cart">
+              <section className={`cart ${selectedImage === 'filter' ? 'sel' : ''}`}>
                 <Link className="cart" to="/Filter">
                   <img
-                    className={`back-button ${selectedImage === 'filter' ? 'sel' : ''}`}
+                    className="back-button"
                     onClick={() => handleImageClick('filter')}
                     src={filter}
                     alt="filter"
@@ -111,10 +112,10 @@ export default function Footer() {
                 </Link>
               </section>
 
-              <section className="cart">
+              <section className={`cart ${selectedImage === 'search' ? 'sel' : ''}`}>
                 <Link className="cart" to="/Search">
                   <img
-                    className={`back-button ${selectedImage === 'search' ? 'sel' : ''}`}
+                    className="back-button"
                     onClick={() => handleImageClick('search')}
                     src={search}
                     alt="search"
@@ -123,9 +124,9 @@ export default function Footer() {
                 </Link>
               </section>
 
-              <section className="cart">
+              <section className={`cart ${showRegistrationForm ? 'sel' : ''}`}>
                 <img
-                  className={`back-button ${showRegistrationForm ? 'sel' : ''}`}
+                  className="back-button"
                   onClick={() => handleImageClick('avatar')}
                   src={ava}
                   alt="avatar"
@@ -146,126 +147,3 @@ export default function Footer() {
     </>
   );
 }
-
-
-// import React, { useState, useEffect, useRef } from 'react';
-// import './footer.css';
-// import { Link, useLocation } from "react-router-dom";
-// import { BooksContext } from '../../BooksContext';
-// import { useIcons } from '../../IconContext';
-// import RegistrationForm from '../cart/RegistrationForm';
-
-// export default function Footer() {
-//   const { theme, cartItems, totalCount, setTotalCount, savedLogin, uiMain, idLoudPrice, showRegistrationForm, setShowRegistrationForm } = React.useContext(BooksContext);
-
-//   const {
-//     filter,
-//     search,    
-//     ava,
-//     carticon,
-//     home,
-//     category, } = useIcons();
-
-//   const [selectedImage, setSelectedImage] = useState(null);  
-
-//   const location = useLocation();
-
-//   const routeToIconMap = useRef({
-//     '/': 'home',
-//     '/BookList': 'category',
-//     '/cart': 'cart',
-//     '/Filter': 'filter',
-//     '/Search': 'search',
-//     '/Form' : 'lol'   
-//   });
-
-//   useEffect(() => {
-//     if (cartItems && cartItems.length > 0) {
-//       setTotalCount(cartItems.reduce((acc, item) => acc + item.count, 0));
-//     } else {
-//       setTotalCount(0);
-//     }
-//   }, [cartItems, setTotalCount]);
-
-//   useEffect(() => {
-//     const iconName = routeToIconMap.current[location.pathname];
-//     if (iconName) {
-//       setSelectedImage(iconName);
-//     }
-//     //console.log('Current location:', location.pathname);
-//   }, [location]);
-
-//   const handleImageClick = (imageName) => {
-//     if (imageName === 'avatar') {
-//         setShowRegistrationForm(prevState => !prevState);        
-//     } else {
-//         setShowRegistrationForm(false);
-//         setSelectedImage(imageName);
-//     }
-// };
- 
-//   return (
-//     <>
-//       {idLoudPrice === uiMain.id && (
-//         <section className={theme}>
-//           <section className="foot">
-//             <section className="footer">
-//               <section className="cart">
-//                 <Link to="/">
-//                   <img className={`back-button ${selectedImage === 'home' ? 'sel' : ''}`} 
-//                   onClick={() => handleImageClick('home')} 
-//                   src={home} alt="home" />
-//                 </Link>
-//               </section>
-
-//               <section className="cart">
-//                 <Link to="/BookList">
-//                   <img className={`back-button ${selectedImage === 'category' ? 'sel' : ''}`} 
-//                   onClick={() => handleImageClick('category')} 
-//                   src={category} alt="category" />
-//                 </Link>
-//               </section>
-
-//               <section className="cart">
-//                 <Link to="/cart">
-//                   <img className={`back-button ${selectedImage === 'cart' ? 'sel' : ''}`} 
-//                   onClick={() => handleImageClick('cart')} 
-//                   src={carticon} alt="cart" />
-//                   {totalCount > 0 && (
-//                     <span className="cartcount rotate"><b>{totalCount}</b></span>
-//                   )}
-//                 </Link>
-//               </section>
-
-//               <section className="cart">
-//                 <Link to="/Filter">
-//                   <img className={`back-button ${selectedImage === 'filter' ? 'sel' : ''}`} 
-//                   onClick={() => handleImageClick('filter')} 
-//                   src={filter} alt="filter" />
-//                 </Link>
-//               </section>
-
-//               <section className="cart">
-//                 <Link to="/Search">
-//                   <img className={`back-button ${selectedImage === 'search' ? 'sel' : ''}`} 
-//                   onClick={() => handleImageClick('search')} 
-//                   src={search} alt="search" />
-//                 </Link>
-//               </section>
-
-//               <section className="cart">               
-//                 <img className={`back-button ${showRegistrationForm ? 'sel' : ''}`} 
-//                 onClick={() => handleImageClick('avatar')} 
-//                 src={ava} alt="avatar" style={{ cursor: 'pointer' }} />
-//                 {savedLogin && (
-//                   <span className="cartcount" translate="no"><strong>{savedLogin.slice(0, 2) + '...'}</strong></span>
-//                 )}
-//               </section>
-//             </section>
-//             {showRegistrationForm && <RegistrationForm />}
-//           </section>
-//         </section>
-//       )}
-//     </>
-//   );
-// }
